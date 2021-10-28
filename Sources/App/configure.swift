02feb6f69bbaa2dc489dbcf177adeebc3f1ffca6
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-28 14:11:00 +0200

    configure monogodb

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,18 +1,21 @@
 
-
 import Leaf
+import MongoDBVapor
 import Vapor
-
-
-
-let directoryConfig = DirectoryConfiguration.detect()
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
+    
+    // Use LeafRender for view
     app.views.use(.leaf)
+    
+    try app.mongoDB.configure(Environment.get("MONGODB_URI") ?? "mongodb+srv://user:password12345@cluster0.v94fl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    
+    ContentConfiguration.global.use(encoder: ExtendedJSONEncoder(), for: .json)
+    ContentConfiguration.global.use(decoder: ExtendedJSONDecoder(), for: .json)
     
     
     // register routes
