1a39f7726908485eb382f17d4833885f4f78fe2d
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-11-01 15:18:59 +0100

    return a list of all store in the database.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,17 +1,15 @@
 import MongoDBVapor
 import Vapor
 
-
-
 extension Request {
-    /// Convenience accessor for the home.kittens collection.
+    /// Convenience accessor for the home.store collection.
     var storeCollection: MongoCollection<Store> {
         self.mongoDB.client.db("storeDb").collection("store", withType: Store.self)
     }
 }
 
 func routes(_ app: Application) throws {
-    // A GET request will return a list of all kittens in the database.
+    // A GET request will return a list of all store in the database.
     app.get { req -> EventLoopFuture<View> in
         req.storeCollection.find().flatMap { cursor in
             cursor.toArray()
@@ -19,13 +17,21 @@ func routes(_ app: Application) throws {
             req.view.render("shop.leaf", ["store": store])
         }
     }
+    
+    /// Handles a request to load the list of store item.
+    app.get("store") { req -> EventLoopFuture<[Store]> in
+        req.storeCollection.find().flatMap { cursor in
+            cursor.toArray()
+        }
+    }
 
-    // A POST request will create a new kitten in the database.
+    // A POST request will create a new store in the database.
     app.post { req -> EventLoopFuture<Response> in
         let newStore = try req.content.decode(Store.self)
         
         return req.storeCollection.insertOne(newStore)
             .map { _ in Response(status: .created) }
     }
+    
 }
 
