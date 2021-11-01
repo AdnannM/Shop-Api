32badd65beb717aa751351c741368baddbcc63a7
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-11-01 13:31:44 +0100

    nista

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -20,18 +20,40 @@ func routes(_ app: Application) throws {
      */
    
     // A GET request will return a list of all store item in the database.
-    app.get { req -> EventLoopFuture<View> in
-        req.storeCollection.find().flatMap { cursor in
-            cursor.toArray()
-        }.flatMap { store in
-            req.view.render("shop.leaf", ["store": store])
+    
+
+    func routes(_ app: Application) throws {
+        /*
+            - TODO:
+                - A POST request will create a new store in the database.
+                - A PUT request will update a store in database
+                - A DELETE request will delete item in database
+         */
+       
+        // A GET request will return a list of all store item in the database.
+        app.get { req -> EventLoopFuture<View> in
+            req.storeCollection.find().flatMap { cursor in
+                cursor.toArray()
+            }.flatMap { store in
+                req.view.render("shop.leaf", ["store": store])
+            }
+        }
+        
+        app.post { req -> EventLoopFuture<Response> in
+            let newStore = try req.content.decode(Store.self)
+            return req.storeCollection.insertOne(newStore).map { _ in Response(status: .created) }
         }
     }
     
+<<<<<<< HEAD
+=======
     app.post { req -> EventLoopFuture<Response> in
         let newStore = try req.content.decode(Store.self)
         return req.storeCollection.insertOne(newStore).map { _ in Response(status: .created) }
     }
+    
+    
+>>>>>>> parent of 1a6d0c3 (fixin form field on webpage)
 }
 
 
